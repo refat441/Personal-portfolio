35db53bf68bda3be6ef3ffdd2149b77abd7770f2
refatahmed441 <refatahmed441@gmail.com>
2024-11-08 07:18:25 +0600

    img

--- src/info/Info.js
+++ src/info/Info.js
@@ -108,33 +108,33 @@ export const info = {
         {
             title: "Inventory Dashboard",
             live: "https://qibla.netlify.app/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+            source: "https://github.com/refat441", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
         {
             title: "Hajj & Umrah Agency",
             live: "https://qibla.netlify.app/",
             // live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            source: "https://github.com/refat441",
             image: mock2
         },
         {
             title: "Restaurant Front-End",
             live: "https://refat441.github.io/Indian_Restaurant/",
-            source: "https://github.com/paytonjewell",
+            source: "https://github.com/refat441",
             image: mock3
         },
         {
             title: "Zan-Web",
             live: "https://refat441.github.io/Zan-Web/",
-            source: "https://github.com/paytonjewell",
+            source: "https://github.com/refat441",
             image: mock4
         },
-        {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock5
-        }
+        // {
+        //     title: "Project 5",
+        //     live: "https://paytonpierce.dev",
+        //     source: "https://github.com/paytonjewell",
+        //     image: mock5
+        // }
     ]
 }
